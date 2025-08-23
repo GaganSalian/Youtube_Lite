9b14d54aa99263958c84878286819a86fb5b3eff
GaganSalian <gagansalian04102005@gmail.com>
2025-08-23 18:28:07 +0530

    Update videoCards.js

--- src/components/videoCards.js
+++ src/components/videoCards.js
@@ -26,7 +26,10 @@ const VideoCards = ({ info }) => {
 
 export const AdVideoCards = ({ info }) => {
   // Just render like normal VideoCard
-  return <VideoCards info={info} />;
+  return (<div className='p1 m-1 border border-black'>
+      <VideoCards info={info} />
+  </div>) 
+
 };
 
 export default VideoCards;
